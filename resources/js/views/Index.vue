b8a308ff45f70fe8544ee1d4894d56721508ada5
MateaMilicevic <mateaaa.milicevic@gmail.com>
2019-11-17 14:35:13 +0100

    novo

--- resources/js/views/Index.vue
+++ resources/js/views/Index.vue
@@ -1,6 +1,7 @@
 <template>
-        <div>
-            <b-container fluid id="main">
+    <div>
+        <b-container fluid id="main">
+            <b-container id="pocetna">
                 <b-row>
                     <b-col cols="12">
                         <div class="viewpoint">
@@ -10,47 +11,41 @@
                         </div>
                     </b-col>
                 </b-row>
-                <b-row id="Onama">
-                    <b-col sm  offset-sm="1" >
-                        <div class="resize-center" >
-                            <blockquote class="blockquote">
-
-                                <h2>VILLA PROMAJNA</h2>
-
-                                <footer class="blockquote-footer">Mjesto odmora i relaksacije</footer>
-
-                            </blockquote>
-
-                                    Promajna je mjesto u općini Baška Voda, u Splitsko-dalmatinskoj županiji,
-                                    smješteno između mjesta Bratuš na istoku i Baška Voda na zapadu.
-                                    Mjesto u cijelosti leži ispod Jadranske magistrale.
-                                    Prema popisu stanovništva iz 2011. godine, u Promajni živi 372 stanovnika.
-                        </div>
-                    </b-col >
-
-                    <b-col sm>
-                        <div class="sort resize-center" >
-                            <b-carousel
-                                id="carousel-1"
-                                style="text-shadow: 0px 0px 2px #000"
-                                fade
-                                :interval="3000"
-                                no-hover-pause
-                                indicators
-                                img-width="1024"
-                                img-height="480"
-                            >
-                                <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=10"></b-carousel-slide>
-                                <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=12"></b-carousel-slide>
-                                <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=22" ></b-carousel-slide>
-                            </b-carousel>
-
-
-                        </div>
-                        <div>
-
-                            <b-button v-b-toggle.collapse-1 block variant="outline-dark" align-self="center" >Otvorite cijelu galeriju</b-button>
-
+            </b-container>
+        </b-container>
+        <b-container fluid id="promajna">
+            <b-row id="Onama">
+                <b-col sm  offset-sm="1" >
+                    <div class="resize-center" >
+                        <blockquote class="blockquote">
+                            <h2>PROMAJNA</h2>
+                            <footer class="blockquote-footer">Mjesto odmora i relaksacije</footer>
+                        </blockquote>
+                            Promajna je mjesto u općini Baška Voda, u Splitsko-dalmatinskoj županiji,
+                            smješteno između mjesta Bratuš na istoku i Baška Voda na zapadu.
+                            Mjesto u cijelosti leži ispod Jadranske magistrale.
+                            Prema popisu stanovništva iz 2011. godine, u Promajni živi 372 stanovnika.
+                    </div>
+                </b-col >
+                <b-col sm>
+                    <div class="sort resize-center" >
+                    <b-carousel
+                        id="carousel-1"
+                        style="text-shadow: 0px 0px 2px #000"
+                        fade
+                        :interval="3000"
+                        no-hover-pause
+                        indicators
+                        img-width="1024"
+                        img-height="480"
+                    >
+                        <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=10"></b-carousel-slide>
+                        <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=12"></b-carousel-slide>
+                        <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=22" ></b-carousel-slide>
+                    </b-carousel>
+                    </div>
+                    <div>
+                        <b-button v-b-toggle.collapse-1 block variant="outline-dark" align-self="center" >Otvorite cijelu galeriju</b-button>
                             <b-collapse id="collapse-1" class="mt-2">
                                 <b-card>
                                     <gallery :images="images" :index="index" @close="index = null"></gallery>
@@ -71,39 +66,54 @@
                                 </b-card>
                             </b-collapse>
                         </div>
-                    </b-col>
-                </b-row>
-
-       
+                </b-col>
+            </b-row>
+        </b-container>
+        
+        <b-container fluid id="apartmani">       
             <b-row>
-                    <b-col>
+                <b-col>
+                    <div class="app mb-3 text-white" >
+                        <b-card-group columns id="kartice">
+                        <b-card v-for="apartment in apartments" :key="apartment.id"
+                        :title= 'apartment.name'
+                        :img-src="'/uploads/' + apartment.name + '/' + apartment.images[0].path"
+                        img-alt="Image"
+                        img-top
+                       bg
+                        tag="article"
+                        style="width: 20rem; background-color: #333333 "
+                        class="mb-3"
+                        
 
-                         <div class="app">
-                            <b-card-group columns>
-                            <b-card v-for="apartment in apartments" :key="apartment.id"
-                            :title= 'apartment.name'
-                            :img-src="'/uploads/' + apartment.name + '/' + apartment.images[0].path"
-                            img-alt="Image"
-                            img-top
-                            tag="article"
-                            style="max-width: 20rem;"
-                            class="mb-2"
-
-                    >
+                        >
                             <b-card-text>
 
                             </b-card-text>
                             <router-link :to="'/app/EditApartment/' +apartment.id"  v-slot="{href, route, navigate}">
-                            <b-button :href="href" variant="primary" @click="navigate">Uredi apartman</b-button> </router-link>
+                            <b-button class="button" :href="href" variant="primary" @click="navigate">Uredi apartman</b-button> </router-link>
                             </b-card>
                             </b-card-group>
                             
-                        </div>
+                    </div>
                     </b-col>
                 </b-row>
-          
-            </b-container>
-        </div>
+        </b-container>
+        <b-container fluid class="Kontakt">
+            <b-row>
+                <b-col>
+                    <div class="ime">
+                        <h3> Villa Promajna</h3>
+                    </div>
+                    <h5> Vlasnik: Mladen Miličević</h5>
+                </b-col>
+                <b-col>
+                </b-col>
+                <b-col>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
 
 
 
@@ -165,9 +175,6 @@ methods: {
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Dancing+Script:400,700&display=swap');
-#apartmani{
-    background-color: #262626;
-}
     .ime{
           font-family: 'Dancing Script', cursive;
           text-align: center;
@@ -175,10 +182,12 @@ methods: {
     h1{
         font-size: 5rem !important;
     }
-    .viewpoint{
+    #main{
+        background: url(../../img/promajna3.jpeg) no-repeat center;
+    }
+    #pocetna{
       
     padding-top:56px;
-    background: url(../../img/promajna3.jpeg) no-repeat center;
     height:100vh;
     -webkit-background-size: cover; /* For WebKit*/
     -moz-background-size: cover;    /* Mozilla*/
@@ -205,7 +214,7 @@ methods: {
     .card-columns{
         height: 100vh;
         width: 100%;
-        background-color: #262626 !important;
+        
 
     }
 }
@@ -215,6 +224,28 @@ methods: {
 }
 .Apartments{
     height:"70vh";
+}
+#apartmani{
+    background-color: #333333;
+    padding-top:30px;
+    padding-left: 30px;
+    //padding-right:30px;
+    padding-bottom:30px;
+}
+
+
+.button {
+  color: white !important; 
+  background-color: #666666 !important; 
+  border-color: #666666 !important;
+}
+.Kontakt{
+    background-color: #262626;
+    color:white;      
+    height:50vh;
+    width:auto;  
+    padding: 30px;
+
 }
 
 </style>
